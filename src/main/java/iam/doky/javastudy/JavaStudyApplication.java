1237bb1a4cdf9a77dcde8e287c18eeaf26666fbe
freed <doky.kim@freedgrouptech.com>
2022-07-21 23:07:31 +0900

    Initial commit

---
+++ src/main/java/iam/doky/javastudy/JavaStudyApplication.java
@@ -0,0 +1,16 @@
+package iam.doky.javastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStudyApplication.class, args);
+
+		System.out.println("hello world");
+
+	}
+
+}
